96b8ebf0260bb24d8ab9a35af00caf34e5cde17a
florence <loracodes3@gmail.com>
2024-10-30 13:39:41 +0300

    accessing elements in an array we use index which starts from 0

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let fruits =["apples", "pineapples" , "watermelon" , "cherry"] //literal notation
 
 let numbers = new Array (1,30,18,67,9)
+
+//ACCESSING ELEMENTS IN AN ARRAY, WE USE INDEX WHICH STARTS FROM 0
+console.log(fruits[0]) //it will output apples
+console.log(numbers[2]) //it will output 18
