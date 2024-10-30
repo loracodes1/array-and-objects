d784a5e2b24fcdb97427c74de3670d06e094275d
florence <loracodes3@gmail.com>
2024-10-30 13:50:22 +0300

    change the name in array at a particular position to another name

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ let numbers = new Array (1,30,18,67,9)
 //ACCESSING ELEMENTS IN AN ARRAY, WE USE INDEX WHICH STARTS FROM 0
 console.log(fruits[0]) //it will output apples
 console.log(numbers[2]) //it will output 18
+
+//modifying eleemnts in an array
+fruits[1]="blueberry" // changes the fruit in the second position which is index 1 to blueberry
+console.log(fruits) // the output will be [ 'apples', 'blueberry', 'watermelon', 'cherry' ]
+
